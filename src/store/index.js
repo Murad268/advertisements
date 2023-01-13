31ec8104c2309f482bc2a69ebaf40ee9647119ec
user.name <user.email>
2023-01-13 11:32:26 +0400

    ss

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 import anq  from './modules/anq'
+import goods from './modules/goods'
 export default createStore({
 
   modules: {
-    anq
+    anq,
+    goods
   }
 })
